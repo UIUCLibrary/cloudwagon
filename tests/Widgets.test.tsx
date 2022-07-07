081e23b9b3dc3c8bc7f84c8867796f27ee79f18c
henry <henryborchers@yahoo.com>
2022-07-07 15:44:22 -0500

    context given for SelectOption widgets

--- tests/Widgets.test.tsx
+++ tests/Widgets.test.tsx
@@ -1,9 +1,10 @@
 import {render} from '@testing-library/react';
 import {SelectOption} from '../frontend/src/Widgets'
-it('Label is written', function () {
-  const {getByLabelText}  = render(
-    <SelectOption label="tester" parameters={{'selections': []}}/>
-  )
-  expect(getByLabelText('tester'))
-
-});
+describe('SelectOption', ()=>{
+  it('Label is written', function () {
+    const {getByLabelText}  = render(
+      <SelectOption label="tester" parameters={{'selections': []}}/>
+    )
+    expect(getByLabelText('tester')).toBeInTheDocument()
+  });
+})
