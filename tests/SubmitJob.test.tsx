f16342b585dd15a90b8b2eddecf6e021c8258e05
henry <henryborchers@yahoo.com>
2022-07-08 11:09:25 -0500

    populate workflows test

--- tests/SubmitJob.test.tsx
+++ tests/SubmitJob.test.tsx
@@ -1,4 +1,38 @@
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react';
 import SubmitJob from '../frontend/src/SubmitJob';
-test('dummy', ()=>{
-  expect(1).toBe(1)
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+describe('s', () => {
+  beforeEach(() => {
+
+    mockedAxios.get.mockResolvedValue(
+        {
+          data:
+              {
+                workflows: [
+                  'Dummy Workflow',
+                ]
+              }
+        })
+  });
+  test('populate workflows', async () => {
+
+    render(
+        <SubmitJob/>
+    );
+    await waitFor(() => {
+      return waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+    });
+
+    fireEvent.mouseDown(screen.getAllByRole('button')[0])
+    expect(screen.getByText('Dummy Workflow')).toBeInTheDocument()
+  })
 })
