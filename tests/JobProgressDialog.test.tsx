407587a7d6caf4d35739f2cfd0d87cedece67e3a
henry <henryborchers@yahoo.com>
2023-02-09 15:00:11 -0600

    JobProgressDialog.test.tsx

--- tests/JobProgressDialog.test.tsx
+++ tests/JobProgressDialog.test.tsx
@@ -2,15 +2,16 @@ import {asyncIterableFromStream} from '../frontend/src/JobProgressDialog'
 import {waitFor} from '@testing-library/react';
 describe('asyncIterableFromStream', ()=>{
   const mockedStream = {
-    read: () => {return Promise.resolve({done: true})},
+    read: () => {
+      const response: ReadableStreamReadResult<string> = {done: true}
+      return Promise.resolve(response)
+    },
     releaseLock:  () => {},
   } as jest.Mocked<ReadableStreamDefaultReader<string>> ;
   test('releaseLock is called', async ()=>{
     jest.spyOn(mockedStream,'releaseLock')
     await waitFor(async ()=>{
-      for await (const num of asyncIterableFromStream(mockedStream)){
-
-      }
+      for await (const num of asyncIterableFromStream(mockedStream)){}
     })
     expect(mockedStream.releaseLock).toHaveBeenCalled()
   })
