5922a989f2ee561f1e23ed0fbfcec5221bd402d5
henry <henryborchers@yahoo.com>
2023-06-15 14:49:59 -0500

    config file can be generated with the create-default-config subcommand

--- src/backend/speedcloud/config.py
+++ src/backend/speedcloud/config.py
@@ -6,6 +6,7 @@ import logging
 
 from pydantic import BaseSettings
 import tomlkit
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,9 +15,9 @@ class Settings(BaseSettings):
 
 
 search_locations: List[str] = [
-        os.getcwd(),
-        os.path.join("/", "opt", "etc", "speedcloud"),
-        os.path.join("/", "etc", "speedcloud")
+    os.getcwd(),
+    os.path.join("/", "opt", "etc", "speedcloud"),
+    os.path.join("/", "etc", "speedcloud"),
 ]
 
 
@@ -26,7 +27,7 @@ def read_settings(config_file: str) -> Settings:
     with open(config_file, "r") as handel:
         data: Dict[str, Any] = tomlkit.parse(handel.read())
 
-    return Settings(storage=data['main']['storage_path'])
+    return Settings(storage=data["main"]["storage_path"])
 
 
 @lru_cache()
@@ -36,10 +37,8 @@ def get_settings() -> Settings:
 
 
 def find_config_file(
-        config_file_name="config.toml",
-        search_paths: Optional[List[str]] = None
+    config_file_name="config.toml", search_paths: Optional[List[str]] = None
 ) -> str:
-
     for location in search_paths or search_locations:
         candidate = os.path.join(location, config_file_name)
         if os.path.exists(candidate):
@@ -47,33 +46,40 @@ def find_config_file(
 
     raise FileNotFoundError("No config file located")
 
+
 class InputPrompt(Protocol):
     def __call__(self, prompt: str, required: bool = False) -> str:
         ...
 
+
 def input_prompt(prompt: str, required: bool = False) -> str:
     while True:
         response = input(prompt)
-        if required and response == '':
+        if required and response == "":
             continue
         return response
 
-def generate_default_toml_config(prompt_for_input: InputPrompt=input_prompt) -> str:
+
+def generate_default_toml_config(
+    prompt_for_input: InputPrompt = input_prompt,
+) -> str:
     main = tomlkit.table()
     doc = tomlkit.document()
-    doc['main'] = main
+    doc["main"] = main
     main.add(
         "storage_path",
-        prompt_for_input('root path to use for files: ', required=True)
+        prompt_for_input("root path to use for files: ", required=True),
     )
-    main['storage_path'].comment(
+    main["storage_path"].comment(
         "storage_path is the root path to use for files read and written to "
         "by the workflows. This is a required field."
     )
     return tomlkit.dumps(doc)
 
 
-def write_default_config_file(file_name, config_file_strategy: Callable[[], str]) -> None:
+def write_default_config_file(
+    file_name, config_file_strategy: Callable[[], str]
+) -> None:
     text = config_file_strategy()
     with open(file_name, "w", encoding="utf-8") as file_handel:
         file_handel.write(text)
--- src/backend/speedcloud/main.py
+++ src/backend/speedcloud/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
@@ -37,29 +37,34 @@ def handel_cloudwagon_exceptions(request: Request, ext: CloudWagonException):
 
 
 app.add_exception_handler(
-    CloudWagonException,
-    handler=handel_cloudwagon_exceptions
+    CloudWagonException, handler=handel_cloudwagon_exceptions
 )
 
 
 def create_default_config_file(args):
-    config.write_default_config_file('config.toml', config_file_strategy=config.generate_default_toml_config)
+    config.write_default_config_file(
+        "config.toml", config_file_strategy=config.generate_default_toml_config
+    )
 
 
 def run_debug(args):
     import uvicorn  # pylint: disable=import-outside-toplevel
+
     logger.warning(
         "IMPORTANT: This is not a production mode! "
         "Use this mode for debugging only."
     )
-    uvicorn.run(app, host='0.0.0.0', port=args.port, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=args.port, log_level="debug")
+
 
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.set_defaults(func=run_debug)
-    subparsers = parser.add_subparsers(help='commands')
+    subparsers = parser.add_subparsers(help="commands")
 
-    create_default_config_parser = subparsers.add_parser('create-default-config', help='create default config file')
+    create_default_config_parser = subparsers.add_parser(
+        "create-default-config", help="create default config file"
+    )
     create_default_config_parser.set_defaults(func=create_default_config_file)
 
     parser.add_argument("--port", type=int, default=8001)
@@ -73,5 +78,5 @@ def main() -> None:
     exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
