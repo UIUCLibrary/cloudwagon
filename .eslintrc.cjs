4d63b7bf1eb79b4bf5e3095e186a7c6b13234409
henry <henryborchers@yahoo.com>
2023-07-13 14:17:44 -0500

    CI: add sonarcloud support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "plugin:sonarjs/recommended"],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'sonarjs'],
   root: true,
 };
 
