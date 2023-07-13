801a6d1bfe3057bbc6454dded2c28ac3e3f010fc
henry <henryborchers@yahoo.com>
2023-07-13 12:36:53 -0500

    CI: GitHub actions - only install the correct browser for playwright test

--- global.teardown.ts
+++ global.teardown.ts
@@ -1,8 +1,22 @@
-import { chromium, FullConfig } from '@playwright/test';
+import { chromium, firefox, webkit, FullConfig } from '@playwright/test';
+import {Browser} from "playwright-core";
 
 export default async function globalTeardown(config: FullConfig){
+    // This cleans out storage for any files generated here.
+    // There is most likely a better way to do this. I just have time to figure it out.
     const { baseURL} = config.projects[0].use;
-    const browser = await chromium.launch();
+    let browser: Browser;
+    try {
+        browser = await chromium.launch();
+
+    } catch (error){
+        try {
+            browser = await firefox.launch()
+
+        } catch (error){
+            browser = await webkit.launch()
+        }
+    }
     const page = await browser.newPage();
     await page.goto(`${baseURL}/manageFiles/?path=/`);
     await page.getByRole('menuitem', {name: "Remove All Files"}).click()
